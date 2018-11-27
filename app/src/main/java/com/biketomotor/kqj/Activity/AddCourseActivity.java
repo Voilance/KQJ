84795ed432d1c078460a741f7ca456f03e6dea54
Voilance <voilance@qq.com>
2018-11-27 23:07:22 +0800

    重构完添加课程

--- app/src/main/java/com/biketomotor/kqj/Activity/AddCourseActivity.java
+++ app/src/main/java/com/biketomotor/kqj/Activity/AddCourseActivity.java
@@ -29,8 +29,8 @@ public class AddCourseActivity extends BaseActivity implements View.OnClickListe
     private EditText etTeacher;
     private TextView tvSelectWeeks;
     private TextView tvSelectWeekdays;
-    private TextView tvStartTime;
-    private TextView tvEndTime;
+    private TextView tvStartLesson;
+    private TextView tvEndLesson;
     private Button btAddCourse;
 
     private final String[] weeks = {
@@ -45,7 +45,7 @@ public class AddCourseActivity extends BaseActivity implements View.OnClickListe
             false, false, false, false, false};
     private final String[] weekdays = {
             "星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期天"};
-    private final String[] Lessons = {
+    private final String[] lessons = {
             "第1节", "第2节", "第3节", "第4节", "第5节", "第6节",
             "第7节", "第8节", "第9节", "第10节", "第11节"};
 
@@ -54,10 +54,13 @@ public class AddCourseActivity extends BaseActivity implements View.OnClickListe
     private AlertDialog startLessonDialog;
     private AlertDialog endLessonDialog;
 
+    private String courseName;
+    private String classroom;
+    private String teacher;
     private final ArrayList<Integer> selectedWeeks = new ArrayList<>(0);
     private Integer selectedWeekday;
-    private Integer startLesson;
-    private Integer endLesson;
+    private Integer selectedStartLesson;
+    private Integer selectedEndLesson;
 
 
     @Override
@@ -79,18 +82,18 @@ public class AddCourseActivity extends BaseActivity implements View.OnClickListe
         etTeacher = findViewById(R.id.et_teacher);
         tvSelectWeeks = findViewById(R.id.tv_select_weeks);
         tvSelectWeekdays = findViewById(R.id.tv_select_weekdays);
-        tvStartTime = findViewById(R.id.tv_start_time);
-        tvEndTime = findViewById(R.id.tv_end_time);
+        tvStartLesson = findViewById(R.id.tv_start_lesson);
+        tvEndLesson = findViewById(R.id.tv_end_lesson);
         btAddCourse = findViewById(R.id.bt_add_course);
         tvSelectWeeks.setOnClickListener(this);
         tvSelectWeekdays.setOnClickListener(this);
-        tvStartTime.setOnClickListener(this);
-        tvEndTime.setOnClickListener(this);
+        tvStartLesson.setOnClickListener(this);
+        tvEndLesson.setOnClickListener(this);
         btAddCourse.setOnClickListener(this);
 
         selectedWeekday = -1;
-        startLesson = -1;
-        endLesson = -1;
+        selectedStartLesson = -1;
+        selectedEndLesson = -1;
     }
 
     @Override
@@ -100,13 +103,18 @@ public class AddCourseActivity extends BaseActivity implements View.OnClickListe
                 selectWeeks();
                 break;
             case R.id.tv_select_weekdays:
+                selectWeekdays();
                 break;
-            case R.id.tv_start_time:
+            case R.id.tv_start_lesson:
+                selectStartLesson();
                 break;
-            case R.id.tv_end_time:
+            case R.id.tv_end_lesson:
+                selectEndLesson();
                 break;
             case R.id.bt_add_course:
-                Log.e(TAG, "onClick: " + selectedWeeks.toString());
+                if (isInfoValid()) {
+                    addCourse();
+                }
                 break;
             default:
                 break;
@@ -135,13 +143,130 @@ public class AddCourseActivity extends BaseActivity implements View.OnClickListe
                         selectedWeeks.add(j + 1);
                     }
                 }
+                if (selectedWeeks.size() > 0) {
+                    tvSelectWeeks.setText("共" + String.valueOf(selectedWeeks.size()) + "周");
+                } else {
+                    tvSelectWeeks.setText("");
+                }
             }
         }).setNegativeButton("取消", null);
         weeksDialog = builder.create();
         weeksDialog.show();
     }
 
+    private void selectWeekdays() {
+        final Integer temp = selectedWeekday;
+        AlertDialog.Builder builder = new AlertDialog.Builder(AddCourseActivity.this);
+        builder.setTitle("选择上课周天");
+        builder.setSingleChoiceItems(weekdays, selectedWeekday, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                selectedWeekday = which;
+            }
+        }).setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                tvSelectWeekdays.setText(weekdays[selectedWeekday]);
+            }
+        }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                selectedWeekday = temp;
+            }
+        });
+        weekdaysDialog = builder.create();
+        weekdaysDialog.show();
+    }
+
+    private void selectStartLesson() {
+        final Integer temp = selectedStartLesson;
+        AlertDialog.Builder builder = new AlertDialog.Builder(AddCourseActivity.this);
+        builder.setTitle("选择上课节次");
+        builder.setSingleChoiceItems(lessons, selectedStartLesson, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                selectedStartLesson = which;
+            }
+        }).setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                tvStartLesson.setText(lessons[selectedStartLesson]);
+            }
+        }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                selectedStartLesson = temp;
+            }
+        });
+        startLessonDialog = builder.create();
+        startLessonDialog.show();
+    }
+
+    private void selectEndLesson() {
+        final Integer temp = selectedEndLesson;
+        AlertDialog.Builder builder = new AlertDialog.Builder(AddCourseActivity.this);
+        builder.setTitle("选择上课节次");
+        builder.setSingleChoiceItems(lessons, selectedEndLesson, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                selectedEndLesson = which;
+            }
+        }).setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                tvEndLesson.setText(lessons[selectedEndLesson]);
+            }
+        }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                selectedEndLesson = temp;
+            }
+        });
+        endLessonDialog = builder.create();
+        endLessonDialog.show();
+    }
+
+    private boolean isInfoValid() {
+        courseName = etCourseName.getText().toString().trim();
+        if (courseName.length() == 0) {
+            toast("请输入课程名称");
+            return false;
+        }
+        classroom = etClassroom.getText().toString().trim();
+        if (classroom.length() == 0) {
+            toast("请输入上课教室");
+            return false;
+        }
+        teacher = etTeacher.getText().toString().trim();
+        if (teacher.length() == 0) {
+            toast("请输入任课教室");
+            return false;
+        }
+        if (selectedWeeks.size() == 0) {
+            toast("请输入上课周数");
+            return false;
+        }
+        if (selectedWeekday < 0) {
+            toast("请输入上课周天");
+            return false;
+        }
+        if (selectedStartLesson < 0) {
+            toast("请输入上课节次");
+            return false;
+        }
+        if (selectedEndLesson < 0) {
+            toast("请输入下课节次");
+            return false;
+        }
+        if (selectedEndLesson < selectedStartLesson) {
+            toast("下课节次不应该早于上课节次");
+            return false;
+        }
+        return true;
+    }
+
     private void addCourse() {
+//        Log.e(TAG, "addCourse: " + getJsonData().toString());
         HttpsUtil.sendPostRequest(HttpsUtil.addCourseAddr, getJsonData(), new HttpsListener() {
             @Override
             public void onSuccess(final String response) {
@@ -149,6 +274,20 @@ public class AddCourseActivity extends BaseActivity implements View.OnClickListe
                     @Override
                     public void run() {
                         Log.e(TAG, response);
+                        try {
+                            JSONObject data = new JSONObject(response);
+                            String result = data.getString("result");
+                            String reason = data.getString("reason");
+                            if (result.equals("true")) {
+                                toast("添加成功");
+                                MainActivity.editView();
+                                finish();
+                            } else {
+                                toast(reason);
+                            }
+                        } catch (JSONException e) {
+                            Log.e(TAG, "addCourse/onSuccess:" + e.toString());
+                        }
                     }
                 });
             }
@@ -165,6 +304,14 @@ public class AddCourseActivity extends BaseActivity implements View.OnClickListe
         try {
             data.put("stu_id", User.getAccount());
             JSONObject courseData = new JSONObject();
+            courseData.put("courseName", courseName);
+            courseData.put("classroom", classroom);
+            courseData.put("teacher", teacher);
+            courseData.put("weeks", selectedWeeks);
+            courseData.put("weekDay", selectedWeekday + 1);
+            courseData.put("startTime", selectedStartLesson + 1);
+            courseData.put("endTime", selectedEndLesson + 1);
+            data.put("courseData", courseData);
         } catch (JSONException e) {
             Log.e(TAG, "getJsonData:" + e.toString());
         }
